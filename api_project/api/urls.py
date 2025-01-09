52ce3da2fc1f54dd1757c5817282816de624728c
jeddy2023 <jkenosuh@gmail.com>
2025-01-09 11:15:45 +0100

    Add BookList API endpoint

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,14 +1,12 @@
-from django.urls import path, include  # Include the include function
-from rest_framework.routers import DefaultRouter  # Import DefaultRouter
-from .views import BookViewSet, BookList  # Import BookViewSet and BookList
+from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
 
-# Initialize the DefaultRouter
 router = DefaultRouter()
-
-# Register the BookViewSet with the router
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),  # Route for the BookList view
-    path('', include(router.urls)),  # Include the router-generated routes
+    path('api-token-auth/', obtain_auth_token, name='api-token-auth'),  # Token retrieval endpoint
+    path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
 from .models import Book  # Import the Book model
 from .serializers import BookSerializer  # Import the BookSerializer
+from rest_framework import permissions
+from .permissions import IsAdminOrReadOnly
 
 class BookViewSet(viewsets.ModelViewSet):  # Extend ModelViewSet
     queryset = Book.objects.all()  # QuerySet for the Book model
     serializer_class = BookSerializer  # Serializer for the Book model
+    permission_classes = [permissions.IsAuthenticated, IsAdminOrReadOnly]
