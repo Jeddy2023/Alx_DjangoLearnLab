6df2de64f954832e0d1da1af76048fceaa9fb9e9
jeddy2023 <jkenosuh@gmail.com>
2025-01-09 11:10:14 +0100

    Add BookList API endpoint

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
-from rest_framework.viewsets import ModelViewSet  # Import ModelViewSet
+from rest_framework import viewsets
 from .models import Book  # Import the Book model
 from .serializers import BookSerializer  # Import the BookSerializer
 
-class BookViewSet(ModelViewSet):  # Extend ModelViewSet
+class BookViewSet(viewsets.ModelViewSet):  # Extend ModelViewSet
     queryset = Book.objects.all()  # QuerySet for the Book model
     serializer_class = BookSerializer  # Serializer for the Book model
