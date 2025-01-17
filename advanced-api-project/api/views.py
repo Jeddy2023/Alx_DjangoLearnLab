13a759c1d753f77771dccc91d0c34313b86e472c
jeddy2023 <jkenosuh@gmail.com>
2025-01-17 04:14:46 +0100

    checking

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+class BookListView(generics.ListCreateAPIView):
+    """
+    Handles the listing of all books and the creation of a new book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [AllowAny]
 
-# Create your views here.
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Handles the retrieval, updating, and deletion of a specific book by ID.
+    Only authenticated users can update or delete books.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
